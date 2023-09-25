8cd891f2588b4bf25d448cc85a51c2917bd6002c
B Salinas <b.salinas397@gmail.com>
2023-09-25 01:41:24 -0500

    made a torusgit add . i feel unstoppable right now

--- multi_cubes.js
+++ multi_cubes.js
@@ -62,9 +62,15 @@ document.body.appendChild(renderer.domElement);
 //   z_rotation,
 // };
 
+const tiny_geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
 const small_geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
 const medium_geometry = new THREE.BoxGeometry(1, 1, 1);
 const large_geometry = new THREE.BoxGeometry(1.7, 1.7, 1.7);
+
+const tiny_material = new THREE.MeshBasicMaterial({
+  color: 0xfdfefe, // white
+  // wireframe: true,
+});
 
 const small_material = new THREE.MeshBasicMaterial({
   color: 0xfdfefe, // white
@@ -72,23 +78,25 @@ const small_material = new THREE.MeshBasicMaterial({
 });
 
 const medium_material = new THREE.MeshBasicMaterial({
-  // color: 0xaab7b8, // grey
-  color: 0xfdfefe, // white
+  color: 0xaab7b8, // grey
+  // color: 0xfdfefe, // white
   wireframe: true,
 });
 
 const large_material = new THREE.MeshBasicMaterial({
-  // color: 0x2e4053, // dark navy blue
-  color: 0xfdfefe, // white
+  color: 0x2e4053, // dark navy blue
+  // color: 0xfdfefe, // white
   wireframe: true,
 });
 
+const tiny_cube = new THREE.Mesh(tiny_geometry, tiny_material);
 const small_cube = new THREE.Mesh(small_geometry, small_material);
 const medium_cube = new THREE.Mesh(medium_geometry, medium_material);
 const large_cube = new THREE.Mesh(large_geometry, large_material);
 
 //
 
+scene.add(tiny_cube);
 scene.add(small_cube);
 scene.add(medium_cube);
 scene.add(large_cube);
@@ -98,17 +106,21 @@ camera.position.z = 3;
 function animate() {
   requestAnimationFrame(animate);
 
-  small_cube.rotation.x += 0.005;
-  small_cube.rotation.y += 0.005;
-  small_cube.rotation.z += 0.005;
+  tiny_cube.rotation.x += 0.011;
+  tiny_cube.rotation.y += 0.011;
+  tiny_cube.rotation.z += 0.011;
 
-  medium_cube.rotation.x += 0.003;
-  medium_cube.rotation.y += 0.003;
-  medium_cube.rotation.z += 0.003;
+  small_cube.rotation.x += 0.007;
+  small_cube.rotation.y += 0.007;
+  small_cube.rotation.z += 0.007;
 
-  large_cube.rotation.x += 0.002;
-  large_cube.rotation.y += 0.002;
-  large_cube.rotation.z += 0.002;
+  medium_cube.rotation.x += 0.005;
+  medium_cube.rotation.y += 0.005;
+  medium_cube.rotation.z += 0.005;
+
+  large_cube.rotation.x += 0.003;
+  large_cube.rotation.y += 0.003;
+  large_cube.rotation.z += 0.003;
 
   renderer.render(scene, camera);
 }
--- torus.js
+++ torus.js
@@ -1,15 +1,58 @@
-import * as THREE from 'three';
-import WebGL from 'three/addons/capabilities/WebGL.js';
+import * as THREE from "three";
+import WebGL from "three/addons/capabilities/WebGL.js";
 
-if (!WebGL.isWebGLAvailable() ) {
+if (!WebGL.isWebGLAvailable()) {
   const warning = WebGL.getWebGLErrorMessage();
-  document.getElementById('contianer').appendChild(warning);
+  document.getElementById("contianer").appendChild(warning);
 }
 
+//
 
-// const aspect = 
+let screen = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-const scene = new THREE.scene();
-const camera = new THREE.PerspectiveCamera()
+const FOV = 100;
+const ASPECT = screen?.width / screen?.height;
+const NEAR = 0.1; // default is 0.1
+const FAR = 1000; // default is 2000
 
+//
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
 const renderer = new THREE.WebGLRenderer();
+
+//
+
+renderer.setSize(screen?.width, screen?.height);
+document.body.appendChild(renderer.domElement);
+
+//
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00bfff,
+  wireframe: true,
+});
+
+const torus = new THREE.Mesh(geometry, material);
+
+//
+
+scene.add(torus);
+
+camera.position.z = 25;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
+  torus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
